e627e57c689068d52c49ad0e32c9317cf5047e24
lloc <re@lloc.de>
2014-09-17 14:56:38 +0200

    Language support added

--- MslsMenu.php
+++ MslsMenu.php
@@ -26,6 +26,26 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+/**
+ * Plugin init
+ * @package mslsmenu
+ */
+function mtw_plugin_init() {
+	load_plugin_textdomain(
+		'mslsmenu',
+		false,
+		dirname( plugin_basename( __FILE__ ) ) . '/languages/'
+	);
+}
+add_action( 'init', 'mtw_plugin_init' );
+
+/**
+ * Callback for wp_nav_menu_items
+ * @package mslsmenu
+ * @param string $items
+ * @param array $args
+ * @return string
+ */
 function mslsmenu_item( $items, $args ) {
 	if ( function_exists( 'the_msls' ) ) {
 		$options = MslsOptions::instance();
@@ -45,13 +65,18 @@ function mslsmenu_item( $items, $args ) {
 }
 add_filter( 'wp_nav_menu_items', 'mslsmenu_item', 10, 2 );
 
+/**
+ * Callback for msls_admin_register
+ * @package mslsmenu
+ * @param string $page
+ */
 function mslsmenu_admin_register( $page ) {
 	add_settings_section( 'mslsmenu_section', __( 'Menu Settings' ), null, $page );
 
 	$args = array( 'msls_admin' => new MslsAdmin() );
 	
-	add_settings_field( 'mslsmenu_theme_location', __( 'Theme Location' ), 'mslsmenu_theme_location', $page, 'mslsmenu_section', $args );
-	add_settings_field( 'mslsmenu_display', __( 'Display' ), 'mslsmenu_display', $page, 'mslsmenu_section', $args );
+	add_settings_field( 'mslsmenu_theme_location', __( 'Theme Location', 'msls' ), 'mslsmenu_theme_location', $page, 'mslsmenu_section', $args );
+	add_settings_field( 'mslsmenu_display', __( 'Display', 'msls' ), 'mslsmenu_display', $page, 'mslsmenu_section', $args );
 	add_settings_field( 'mslsmenu_before_output', __( 'Text/HTML before the list', 'msls' ), 'mslsmenu_before_output', $page, 'mslsmenu_section', $args );
 	add_settings_field( 'mslsmenu_after_output', __( 'Text/HTML after the list', 'msls' ), 'mslsmenu_after_output', $page, 'mslsmenu_section', $args );
 	add_settings_field( 'mslsmenu_before_item', __( 'Text/HTML before each item', 'msls' ), 'mslsmenu_before_item', $page, 'mslsmenu_section', $args );
@@ -59,6 +84,11 @@ function mslsmenu_admin_register( $page ) {
 }
 add_action( 'msls_admin_register', 'mslsmenu_admin_register' );
 
+/**
+ * Callback for mslsmenu_theme_location
+ * @package mslsmenu
+ * @param array $args
+ */
 function mslsmenu_theme_location( $args ) {
 	$locations = array( 0 => '' );
 	foreach ( get_nav_menu_locations() as $key => $value ) {
@@ -72,6 +102,11 @@ function mslsmenu_theme_location( $args ) {
 	); // xss ok
 }
 
+/**
+ * Callback for mslsmenu_display
+ * @package mslsmenu
+ * @param array $args
+ */
 function mslsmenu_display( $args ) {
 	echo $args['msls_admin']->render_select(
 		'mslsmenu_display',
@@ -80,18 +115,38 @@ function mslsmenu_display( $args ) {
 	); // xss ok
 }
 
+/**
+ * Callback for mslsmenu_before_output
+ * @package mslsmenu
+ * @param array $args
+ */
 function mslsmenu_before_output( $args ) {
 	echo $args['msls_admin']->render_input( 'mslsmenu_before_output' ); // xss ok
 }
 
+/**
+ * Callback for mslsmenu_after_output
+ * @package mslsmenu
+ * @param array $args
+ */
 function mslsmenu_after_output( $args ) {
 	echo $args['msls_admin']->render_input( 'mslsmenu_after_output' ); // xss ok
 }
 
+/**
+ * Callback for mslsmenu_before_item
+ * @package mslsmenu
+ * @param array $args
+ */
 function mslsmenu_before_item( $args  ) {
 	echo $args['msls_admin']->render_input( 'mslsmenu_before_item' ); // xss ok
 }
 
+/**
+ * Callback for mslsmenu_after_item
+ * @package mslsmenu
+ * @param array $args
+ */
 function mslsmenu_after_item( $args  ) {
 	echo $args['msls_admin']->render_input( 'mslsmenu_after_item' ); // xss ok
 }
