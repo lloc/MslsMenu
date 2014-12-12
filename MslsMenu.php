052e35066c95fdb00d91145cc409e16a7123273b
lloc <re@lloc.de>
2014-12-12 12:21:26 +0100

    Refactoring B rated method

--- MslsMenu.php
+++ MslsMenu.php
@@ -90,39 +90,25 @@ class MslsMenu {
 
 		$args = array( 'msls_admin' => new MslsAdmin() );
 
-		add_settings_field(	'mslsmenu_theme_location',
-			__( 'Theme Location', 'mslsmenu' ),
-			array( $this, 'theme_location' ),
-			$page,
-			'mslsmenu_section',
-			$args
-		);
-		add_settings_field(
-			'mslsmenu_display',
-			__( 'Display', 'mslsmenu' ),
-			array( $this, 'display' ),
-			$page,
-			'mslsmenu_section',
-			$args
-		);
+		add_settings_field( 'mslsmenu_theme_location', __( 'Theme Location', 'mslsmenu' ), array(
+				$this,
+				'theme_location'
+			), $page, 'mslsmenu_section', $args );
+		add_settings_field( 'mslsmenu_display', __( 'Display', 'mslsmenu' ), array(
+				$this,
+				'display'
+			), $page, 'mslsmenu_section', $args );
 
 		$fields = array(
-			'mslsmenu_before_output' => __( 'Text/HTML before the list', 'mslsmenu' ),
-			'mslsmenu_after_output'  => __( 'Text/HTML after the list', 'mslsmenu' ),
+			'mslsmenu_before_output' => __( 'Text/HTML before the list',  'mslsmenu' ),
+			'mslsmenu_after_output'  => __( 'Text/HTML after the list',   'mslsmenu' ),
 			'mslsmenu_before_item'   => __( 'Text/HTML before each item', 'mslsmenu' ),
-			'mslsmenu_after_item'    => __( 'Text/HTML after each item', 'mslsmenu' ),
+			'mslsmenu_after_item'    => __( 'Text/HTML after each item',  'mslsmenu' ),
 		);
 
 		foreach ( $fields as $id => $label ) {
 			$args['mslsmenu_input'] = $id;
-			add_settings_field(
-				$id,
-				$label,
-				array( $this, 'input' ),
-				$page,
-				'mslsmenu_section',
-				$args
-			);
+			add_settings_field( $id, $label, array( $this, 'input' ), $page, 'mslsmenu_section', $args );
 		}
 	}
 
