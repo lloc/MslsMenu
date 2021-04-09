f64e174acd3f0cc4a02862263c1d1cc3b7558345
Dennis Ploetner <re@lloc.de>
2021-04-09 09:31:18 +0200

    Tests added

--- tests/test-MslsMenu.php
+++ tests/test-MslsMenu.php
@@ -11,15 +11,23 @@ class MslsMenu extends TestCase {
 	protected function setUp(): void {
 		parent::setUp();
 		Monkey\setUp();
+
+		Functions\expect( 'load_plugin_textdomain' );
+		Functions\when( 'plugin_basename' )->justReturn( 'abc' );
 	}
 
 	public function test_init() {
-		Functions\expect( 'load_plugin_textdomain' );
-		Functions\when( 'plugin_basename' )->justReturn( 'abc' );
+		$this->assertInstanceOf( \MslsMenu::class, \MslsMenu::init() );
+	}
 
-		$obj = \MslsMenu::init();
+	public function test_admin_register() {
+		Functions\expect( '__' )->once();
+		Functions\expect( 'add_settings_section' )->once();
 
-		$this->assertInstanceOf( \MslsMenu::class, $obj );
+		$obj = new \MslsMenu();
+
+		$obj->admin_register( 'test' );
+		$this->expectOutputString( '' );
 	}
 
 	protected function tearDown(): void {
