ee00461f9ed7518a0ea77390a8d444b78f251ad0
Dennis Ploetner <re@lloc.de>
2025-10-12 14:36:59 +0200

    Test corrected

--- tests/NullObjectTest.php
+++ tests/NullObjectTest.php
@@ -5,39 +5,58 @@ namespace lloc\MslsMenuTests;
 use Brain\Monkey;
 use Brain\Monkey\Functions;
 
-beforeAll( function () {
-	Monkey\setUp();
+beforeAll(
+	function () {
+		Monkey\setUp();
 
-	Functions\expect( 'load_plugin_textdomain' )->atLeast()->once();
-	Functions\stubs( [
-		'plugin_basename' => 'abc',
-		'__'
-	] );
-} );
+		Functions\stubs(
+			array(
+				'plugin_basename' => 'abc',
+				'__',
+			)
+		);
+	}
+);
 
 
-beforeEach( function () {
-	$this->sut = \MslsMenu::init( null );
-} );
+beforeEach(
+	function () {
+		$this->sut = \MslsMenu::init( null );
+	}
+);
 
-afterAll( function () {
-	Monkey\tearDown();
-} );
+afterAll(
+	function () {
+		Monkey\tearDown();
+	}
+);
 
-it( 'returns an instance of MslsMenu even when factory received null', function () {
-	expect( $this->sut )->toBeInstanceOf( \MslsMenu::class );
-} );
+it(
+	'returns an instance of MslsMenu even when factory received null',
+	function () {
+		expect( $this->sut )->toBeInstanceOf( \MslsMenu::class );
+	}
+);
 
-it( 'adds no filter when factory received null', function() {
-	expect( has_filter( 'wp_nav_menu_items', [ \MslsMenu::class, 'nav_item' ] ) )->toBeFalse();
-} );
+it(
+	'adds no filter when factory received null',
+	function () {
+		expect( has_filter( 'wp_nav_menu_items', array( \MslsMenu::class, 'nav_item' ) ) )->toBeFalse();
+	}
+);
 
-it( 'adds no action when factory received null', function () {
-	expect( has_action( 'msls_admin_register', [ \MslsMenu::class, 'admin_register' ] ) )->toBeFalse();
-} );
+it(
+	'adds no action when factory received null',
+	function () {
+		expect( has_action( 'msls_admin_register', array( \MslsMenu::class, 'admin_register' ) ) )->toBeFalse();
+	}
+);
 
-it( 'returns an empty string on $sut->nav_item() when factory received null', function() {
-	$result = $this->sut->nav_item( '', (object) [] );
+it(
+	'returns an empty string on $sut->nav_item() when factory received null',
+	function () {
+		$result = $this->sut->nav_item( '', (object) array() );
 
-	expect( $result )->toEqual( '' );
-} );
+		expect( $result )->toEqual( '' );
+	}
+);
--- tests/ValidObjectTest.php
+++ tests/ValidObjectTest.php
@@ -5,87 +5,121 @@ namespace lloc\MslsMenuTests;
 use Brain\Monkey;
 use Brain\Monkey\Functions;
 
-beforeAll( function () {
-	Monkey\setUp();
+beforeAll(
+	function () {
+		Monkey\setUp();
 
-	Functions\expect( 'load_plugin_textdomain' )->atLeast()->once();
-	Functions\stubs( [
-		'plugin_basename' => 'abc',
-		'__',
-		'selected' => 'selected="selected"',
-	] );
-} );
+		Functions\stubs(
+			array(
+				'plugin_basename' => 'abc',
+				'__',
+				'selected'        => 'selected="selected"',
+			)
+		);
+	}
+);
 
-beforeEach( function() {
-	$options = \Mockery::mock( 'lloc\Msls\MslsOptions' );
-	$options->mslsmenu_theme_location = [ 'test' ];
+beforeEach(
+	function () {
+		$options                          = \Mockery::mock( 'lloc\Msls\MslsOptions' );
+		$options->mslsmenu_theme_location = array( 'test' );
 
-	$this->sut = \MslsMenu::init( $options );
-} );
+		$this->sut = \MslsMenu::init( $options );
+	}
+);
 
-afterAll( function () {
-	Monkey\tearDown();
-} );
+afterAll(
+	function () {
+		Monkey\tearDown();
+	}
+);
 
-it( 'returns an instance of MslsMenu when factory receives an MslsOptions object', function () {
-	expect( $this->sut )->toBeInstanceOf( \MslsMenu::class );
-} );
+it(
+	'returns an instance of MslsMenu when factory receives an MslsOptions object',
+	function () {
+		expect( $this->sut )->toBeInstanceOf( \MslsMenu::class );
+	}
+);
 
-it( 'adds a filter when factory receives an MslsOptions object', function () {
-	expect( has_filter( 'wp_nav_menu_items', [ $this->sut, 'nav_item' ] ) )->toEqual( 10 );
-} );
+it(
+	'adds a filter when factory receives an MslsOptions object',
+	function () {
+		expect( has_filter( 'wp_nav_menu_items', array( $this->sut, 'nav_item' ) ) )->toEqual( 10 );
+	}
+);
 
-it( 'adds an action when factory receives an MslsOptions object', function () {
-	expect( has_action( 'msls_admin_register', [ $this->sut, 'admin_register' ] ) )->toEqual( 10 );
-} );
+it(
+	'adds an action when factory receives an MslsOptions object',
+	function () {
+		expect( has_action( 'msls_admin_register', array( $this->sut, 'admin_register' ) ) )->toEqual( 10 );
+	}
+);
 
-it( 'calls add_settings_section on $sut->admin_register()', function () {
-	Functions\expect( 'add_settings_section' )->once();
+it(
+	'calls add_settings_section on $sut->admin_register()',
+	function () {
+		Functions\expect( 'add_settings_section' )->once();
 
-	$this->sut->admin_register( 'test' );
-} );
+		$this->sut->admin_register( 'test' );
+	}
+);
 
-it( 'returns an empty string on $sut->nav_item()', function() {
-	$expected = '';
+it(
+	'returns an empty string on $sut->nav_item()',
+	function () {
+		$expected = '';
 
-	$args = new \stdClass();
-	$args->theme_locations = 'test';
+		$args                  = new \stdClass();
+		$args->theme_locations = 'test';
 
-	$result = $this->sut->nav_item( '', $args );
+		$result = $this->sut->nav_item( '', $args );
 
-	expect( $result )->toEqual( $expected );
-} );
+		expect( $result )->toEqual( $expected );
+	}
+);
 
-it( 'calls add_settings_field on $sut->add_settings()', function () {
-	Functions\expect( 'add_settings_field' )->times( 6 );
+it(
+	'calls add_settings_field on $sut->add_settings()',
+	function () {
+		Functions\expect( 'add_settings_field' )->times( 6 );
 
-	$this->sut->add_settings();
-} );
+		$this->sut->add_settings();
+	}
+);
 
-it( 'calls get_nav_menu_locations on $sut->theme_location()', function () {
-	Functions\expect( 'get_nav_menu_locations' )->once()->andReturn( [ 'test' => 1 ] );
-    Functions\expect( 'esc_attr' )->once()->andReturnFirstArg();
-    Functions\expect( 'esc_html__' )->once()->andReturnFirstArg();
+it(
+	'calls get_nav_menu_locations on $sut->theme_location()',
+	function () {
+		Functions\expect( 'get_nav_menu_locations' )->once()->andReturn( array( 'test' => 1 ) );
+		Functions\expect( 'esc_attr' )->once()->andReturnFirstArg();
+		Functions\expect( 'esc_html__' )->once()->andReturnFirstArg();
 
-	$expected = '<select id="mslsmenu_theme_location" name="msls[mslsmenu_theme_location][]" multiple="multiple"><option value="" selected="selected">-- empty --</option><option value="test" selected="selected">test</option></select>';
+		$expected = '<select id="mslsmenu_theme_location" name="msls[mslsmenu_theme_location][]" multiple="multiple"><option value="" selected="selected">-- empty --</option><option value="test" selected="selected">test</option></select>';
 
-	$this->sut->theme_location( [] );
+		$this->sut->theme_location( array() );
 
-	$this->expectOutputString( $expected );
-} );
+		$this->expectOutputString( $expected );
+	}
+);
 
-it( 'prints a string on $sut->display()', function () {
-	$expected = '<select></select>';
+it(
+	'prints a string on $sut->display()',
+	function () {
+		$expected = '<select></select>';
 
-	$this->sut->display( [] );
+		$this->sut->display( array() );
 
-	$this->expectOutputString( $expected );
-} );
+		$this->expectOutputString( $expected );
+	}
+);
 
-it( 'prints a string on $sut->input()', function () {
-	$expected = '<input />';
+it(
+	'prints a string on $sut->input()',
+	function () {
+		$expected = '<input />';
 
-	$this->sut->input( [] );
+		$this->sut->input( array() );
 
-	$this->expectOutputString( $expected );
-} );
+		$this->expectOutputString( $expected );
+	}
+);
